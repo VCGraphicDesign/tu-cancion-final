ab49eb8278da41915639c5be90b387e603e6367c
VCGraphicDesign <g.d.chile@gmail.com>
2025-12-25 16:47:48 -0300

    arreglando botones

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Music, AlertCircle, X, Mail, Lock, User as UserIcon, ArrowRight } from 'lucide-react';
-import { authService } from '../services/mockBackend';
+import { authService } from '../services/firebase';
 import { User } from '../types';
 
 interface AuthProps {
