f0c709a09b0961230449f1221cbbb5824a156454
VCGraphicDesign <g.d.chile@gmail.com>
2025-12-26 16:20:41 -0300

    ruta admin

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -38,7 +38,7 @@ const Layout: React.FC<LayoutProps> = ({ children, user, onLogout }) => {
             </Link>
 
             <Link 
-              to={user && user.email === 'g.d.chile@gmail.com' ? "/admin" : "/auth"} 
+              to={user && user.email === 'g.d.chile@gmail.com' ? "/admin" : "/"}
               className="text-[15px] font-bold text-[#1e5d4d] flex items-center gap-2"
 >
               <LayoutDashboard size={20} />
