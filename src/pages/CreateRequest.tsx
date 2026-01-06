da3fc70c65ed2010c2c6a2fe31222b56f9ad8803
VCGraphicDesign <g.d.chile@gmail.com>
2026-01-06 13:42:54 -0300

    modifica cr

--- src/pages/CreateRequest.tsx
+++ src/pages/CreateRequest.tsx
@@ -96,7 +96,7 @@ const CreateRequest: React.FC<CreateRequestProps> = ({ user }) => {
 
       // 2. Envía email de notificación con todos los campos
       try {
-        const response = await fetch('/api/send-email', {
+        const response = await fetch('/api/send-email.cjs', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
